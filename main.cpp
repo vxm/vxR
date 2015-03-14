a98d41cd2f6d79d827e285862b4178846c23628b
Andy Murray <andrei@aolee.com>
2015-03-14 11:15:08 +0000

    removed long commented code. Included qimage and save an image with no content yet

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "vxrendermain.h"
 #include <QApplication>
 #include <vxrenderprocess.h>
-
+#include <QImage>
 
 int main(int argc, char *argv[])
 {
@@ -12,64 +12,8 @@ int main(int argc, char *argv[])
 	vxCompute::vxRenderProcess rp;
 	rp.execute();
 	
-	/*	QImage qtImage;
-	
-		qtImage.create(QSize(1200,900), 32, 0, QImage::IgnoreEndian);
-	
-		vxCamera cam;
-	
-		cam.set(vxVector3d (0,0,0), vxVector3d (0,0,1), 1, 1.1, 1.333 );
-	 
-		cam.setResolution(1200,900);
-	
-		cam.setSamples(2);
-	
-		int visto=0;
-	
-		int posPixX=0;
-		int posPixY=0;
-	
-		cam.resetRay();
-	
-		vxBoxMatrix mat(entrX->value() ,entrY->value() ,entrZ->value(), 6);
-		mat.createSphere(entrX->value() ,entrY->value() ,entrZ->value(), 2);
-	//	mat.createRandom();
-	
-		vxPixel color;
-	
-		while(!cam.rayIsDone())
-		{
-			color.set(0,0,0);
-	
-			vxCollision collide;
-	
-			cam.resetPixel();
-	
-			while( !cam.pixIsDone() )
-			{
-				mat.throwRay(cam.nextRay(), collide );
-				
-				if (collide.isValid()) 
-				{
-					color.add( collide.getColor() );
-				}
-				else
-				{
-					color.add(0,0,0);
-				}
-			}
-	
-			posPixX=cam.getXCoord();
-			posPixY=cam.getYCoord();				
-		
-			color.setResult();
-			qtImage.setPixel( posPixX , posPixY , qRgba( color.getR(), color.getG() , color.getB(), 255));
-		}
-	
-	
-		actualizeImage(qtImage);
-	
-		qtImage.save(QString("c:/image.bmp"),"BMP");
-	*/
+	QImage img(1200, 900, QImage::Format_RGB32);
+	img.save(QString("image.bmp"),"BMP");
+
 	return a.exec();
 }
