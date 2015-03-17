d747d72cb74c86d76ccd0de8e2da56d9fb5c22c9
Andy Murray <andrei@aolee.com>
2015-03-17 00:55:34 +0000

    added helpers in img properties

--- imageproperties.cpp
+++ imageproperties.cpp
@@ -1,5 +1,86 @@
 #include "imageproperties.h"
 
-ImageProperties::ImageProperties()
+
+
+
+ImgFormat ImageProperties::format() const
 {
+	return m_format;
+}
+
+void ImageProperties::setFormat(const ImgFormat &format)
+{
+	m_format = format;
+}
+
+ImgFormat ImageProperties::channels() const
+{
+	return m_channels;
+}
+
+void ImageProperties::setChannels(const ImgFormat &channels)
+{
+	m_channels = channels;
+}
+
+ImgChannels ImageProperties::channels() const
+{
+	return m_channels;
+}
+
+void ImageProperties::setChannels(const ImgChannels &channels)
+{
+	m_channels = channels;
+}
+
+unsigned int ImageProperties::rx() const
+{
+	return m_rx;
+}
+
+void ImageProperties::setRx(unsigned int rx)
+{
+	m_rx = rx;
+}
+
+unsigned int ImageProperties::ry() const
+{
+	return m_ry;
+}
+
+void ImageProperties::setRy(unsigned int ry)
+{
+	m_ry = ry;
+}
+
+
+unsigned int vxStorage::ImageProperties::numPixels() const
+{
+	return m_rx * m_ry;
+}
+
+unsigned int vxStorage::ImageProperties::numChannels() const
+{
+	switch(m_channels)
+	{
+		case kRGB:
+			return 3;
+		break;
+		case kRGBA:
+			return 4;
+		break;
+		case kRGBZ:
+			return 4;
+		break;
+		case kRGBAZ:
+			return 5;
+		break;
+		default:
+			return 0;
+	};
+}
+
+unsigned int vxStorage::ImageProperties::numElements() const
+{
+	return numPixels() * numChannels();
 }
--- imageproperties.h
+++ imageproperties.h
@@ -1,10 +1,56 @@
 #ifndef IMAGEPROPERTIES_H
 #define IMAGEPROPERTIES_H
 
+namespace vxStorage {
+
 class ImageProperties
 {
-	public:
-		ImageProperties();
+	enum class ImgFormat;
+	enum class ImgChannels;
+	
+	ImgFormat m_format = {ImgFormat::k8};
+	ImgChannels m_channels = {ImgChannels::kRGBA};
+	unsigned int m_rx = {120};
+	unsigned int m_ry = {70};
+	
+public:
+	ImageProperties()
+	{}
+	
+	ImageProperties(const unsigned int rx, const unsigned int ry)
+		: m_rx(rx)
+		, m_ry(ry)
+	{
+	}
+	
+	// defines the size in bits for each channel.
+	enum class ImgFormat {k8, k16, k32, k64};
+	// enumerates the channels to store.
+	enum class ImgChannels {kRGB, kRGBA, kRGBZ, kRGBAZ};
+
+	ImgFormat format() const;
+	void setFormat(const ImgFormat &format);
+
+	ImgChannels channels() const;
+	void setChannels(const ImgChannels &channels);
+	
+	unsigned int rx() const;
+	void setRx(unsigned int rx);
+	
+	unsigned int ry() const;
+	void setRy(unsigned int ry);
+
+	// returns the number of pixels
+	unsigned int numPixels() const;
+	
+	// returns the number of channels
+	unsigned int numChannels() const;
+	
+	// returns the number of values in image
+	// channels multiplied by pixels
+	unsigned int numElements() const;
 };
 
+
+}
 #endif // IMAGEPROPERTIES_H
