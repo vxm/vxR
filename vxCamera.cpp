225f1d501b8aa8c8b3ec2f838564fdd6943bd50d
Andy Murray <andrei@aolee.com>
2015-03-18 22:43:15 +0000

    added getter and setter for image properties

--- vxCamera.cpp
+++ vxCamera.cpp
@@ -1,1 +1,16 @@
-#include "vxCamera.h"
+#include <vxCamera.h>
+#include <imageproperties.h>
+
+using namespace vxStorage;
+
+ImageProperties vxCamera::prop() const
+{
+	return m_prop;
+}
+
+void vxCamera::setProp(const ImageProperties &prop)
+{
+	m_prop = prop;
+}
+
+
