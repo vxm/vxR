f5f7f0b4040efedc65cd3880ea3ea68e4151d24d
Mariano Blanc <marianobm@gmail.com>
2015-04-05 00:51:47 +0100

    moved time utilities to different file

---
+++ vxtimeutilities.cpp
@@ -0,0 +1,5 @@
+#include "vxtimeutilities.h"
+
+vxTimeUtilities::vxTimeUtilities()
+{
+}
---
+++ vxtimeutilities.h
@@ -0,0 +1,10 @@
+#ifndef VXTIMEUTILITIES_H
+#define VXTIMEUTILITIES_H
+
+class vxTimeUtilities
+{
+	public:
+		vxTimeUtilities();
+};
+
+#endif // VXTIMEUTILITIES_H
