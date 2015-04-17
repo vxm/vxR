77abe11f1f8843c9d736e4be033791824be2d03a
Mariano Blanc <marianobm@gmail.com>
2015-04-17 01:59:51 +0100

    project options

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	// Img properties for render.
 	auto imgDesc = 
 			std::make_shared<vxStorage::ImageProperties>
-			(800, 800);
+			(1800, 1800);
 	
 	// create the render process
 	render rp(imgDesc);
