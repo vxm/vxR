5595e7ff396210e1391a90e08e1606295a9a35db
Mariano Blanc <marianobm@gmail.com>
2015-04-22 09:11:53 +0100

    persuing Pimpl benefits, refactor code, moved to cpp from h

--- vxScene.cpp
+++ vxScene.cpp
@@ -27,6 +27,24 @@ void vxScene::setCamera(const std::shared_ptr<vxCamera> &camera)
 {
 	m_camera = camera;
 }
+int vxScene::throwRay(const vxVector3d &ray, vxCollision &collide)
+{ 
+	if(!m_grids.size())
+	{
+		return 0;
+	}
+	
+	m_grids[0]->throwRay(ray,collide);
+	
+	if (collide.isValid())
+	{
+		defaultShader()->getColor(collide);
+		return 1;
+	}
+	
+	return 0;
+}
+
 std::shared_ptr<vxLight> vxScene::defaultLight() const
 {
 	return m_light;
