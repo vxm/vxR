2e8219356119e18a7597e2ab5f53cfa5bdda7635
Mariano Blanc <marianobm@gmail.com>
2015-04-23 08:50:23 +0100

    pimpl implementation, looking for faster compilations

--- vxScene.cpp
+++ vxScene.cpp
@@ -1,5 +1,25 @@
 #include "vxScene.h"
 
+vxScene::vxScene(std::shared_ptr<ImageProperties> prop)
+	: m_prop(prop)
+{
+	m_shader = std::make_shared<vxLambert>();
+	m_light = std::make_shared<vxPointLight>();
+	m_light->setPosition(44, 183, -3);
+	
+	createCamera(vxMatrix(), 1);
+	createGrid();
+}
+
+std::shared_ptr<vxCamera> vxScene::createCamera(const vxMatrix &transform, unsigned int samples, double hAperture, double vAperture)
+{
+	m_camera = std::make_shared<vxCamera>(m_prop);
+	m_camera->set(	vxVector3d(0,0,0),
+					vxVector3d(0,0,1),
+					1.83);
+	m_camera->setPixelSamples(samples);
+	return m_camera;
+}
 
 std::shared_ptr<vxShader> vxScene::defaultShader() const
 {
@@ -27,6 +47,52 @@ void vxScene::setCamera(const std::shared_ptr<vxCamera> &camera)
 {
 	m_camera = camera;
 }
+
+std::shared_ptr<vxGrid> vxScene::createGrid()
+{
+	// this is the grid object
+	const double resl = 5.0;
+	
+	vxVector3d p{resl/1.2, 0.0, resl*2.20};
+	
+	// this is a hardcode program to test the rays. 
+	//TODO:get rid of this hard-coded values.
+	m_grids.push_back(std::make_shared<vxGrid>(p.x(), p.y(), p.z(), resl));
+	//m_grids[0]->setResolution(resl);
+	/*m_grids[0]->createSphere(p.x(), p.y(), p.z(),  (resl/3.0)); // Position, radius		
+	m_grids[0]->createSphere(p.x()+(resl/3.0), p.y(), p.z(),  (resl/6.0)); // Position, radius
+	m_grids[0]->createSphere(p.x()-(resl/3.0), p.y(), p.z(),  (resl/6.0)); // Position, radius
+	m_grids[0]->createSphere(p.x(), p.y()+(resl/3.0), p.z(),  (resl/6.0)); // Position, radius
+	m_grids[0]->createSphere(p.x(), p.y()-(resl/3.0), p.z(),  (resl/6.0)); // Position, radius
+	m_grids[0]->createSphere(p.x(), p.y(), p.z()+(resl/3.0),  (resl/6.0)); // Position, radius
+	m_grids[0]->createSphere(p.x(), p.y(), p.z()-(resl/3.0),  (resl/6.0)); // Position, radius
+	m_grids[0]->createRandom(0.0007);
+	m_grids[0]->createGround();
+	m_grids[0]->createEdges(); // of the grid
+	//#ifdef _DEBUG
+	*/
+	//m_grids[0]->activate(3,3,1);
+	//m_grids[0]->activate(4,0,3);
+	m_grids[0]->activate(3,0,3);
+	
+	auto na = m_grids[0]->numActiveVoxels();
+	auto totals = m_grids[0]->getNumberOfVoxels();
+	std::cout << "Number of active voxels " << na << " of " << totals << std::endl;
+//#endif
+
+	return m_grids[0];
+}
+
+std::shared_ptr<ImageProperties> vxScene::prop() const
+{
+	return m_prop;
+}
+
+void vxScene::setProp(const std::shared_ptr<ImageProperties> &prop)
+{
+	m_prop = prop;
+}
+
 int vxScene::throwRay(const vxVector3d &ray, vxCollision &collide)
 { 
 	if(!m_grids.size())
@@ -36,7 +102,7 @@ int vxScene::throwRay(const vxVector3d &ray, vxCollision &collide)
 	
 	m_grids[0]->throwRay(ray,collide);
 	
-	if (collide.isValid())
+	if(collide.isValid())
 	{
 		defaultShader()->getColor(collide);
 		return 1;
--- vxScene.h
+++ vxScene.h
@@ -31,16 +31,7 @@ protected:
 
 public:
 
-	vxScene(std::shared_ptr<ImageProperties> prop)
-		: m_prop(prop)
-	{
-		m_shader = std::make_shared<vxLambert>();
-		m_light = std::make_shared<vxPointLight>();
-		m_light->setPosition(4, 8, -3);
-		
-		createCamera(vxMatrix(), 2);
-		createGrid();
-	}
+	vxScene(std::shared_ptr<ImageProperties> prop);
 	
 	~vxScene()
 	{
@@ -55,57 +46,14 @@ public:
 		createCamera(const vxMatrix &transform,
 					unsigned int samples = 1,
 					double hAperture = 0.0,
-					double vAperture = 0.0)
-	{
-		m_camera = std::make_shared<vxCamera>(m_prop);
-		m_camera->set(	vxVector3d(0,0,0),
-						vxVector3d(0,0,1),
-									1.23);
-		m_camera->setPixelSamples(samples);
-		return m_camera;
-	}
+					double vAperture = 0.0);
 
-	std::shared_ptr<vxGrid>
-	createGrid()
-	{
-		// this is the grid object
-		const double resl = 15.0;
-
-		vxVector3d p{resl/2.0, -1.0, resl*2.20};
-		
-		// this is a hardcode program to test the rays. 
-		//TODO:get rid of this hard-coded values.
-		m_grids.push_back(std::make_shared<vxGrid>(p.x(), p.y(), p.z(), resl));
-		m_grids[0]->setResolution(resl);
-		m_grids[0]->createSphere(p.x(), p.y(), p.z(),  (resl/3.0)); // Position, radius		
-		m_grids[0]->createSphere(p.x()+(resl/3.0), p.y(), p.z(),  (resl/6.0)); // Position, radius
-		m_grids[0]->createSphere(p.x()-(resl/3.0), p.y(), p.z(),  (resl/6.0)); // Position, radius
-		m_grids[0]->createSphere(p.x(), p.y()+(resl/3.0), p.z(),  (resl/6.0)); // Position, radius
-		m_grids[0]->createSphere(p.x(), p.y()-(resl/3.0), p.z(),  (resl/6.0)); // Position, radius
-		m_grids[0]->createSphere(p.x(), p.y(), p.z()+(resl/3.0),  (resl/6.0)); // Position, radius
-		m_grids[0]->createSphere(p.x(), p.y(), p.z()-(resl/3.0),  (resl/6.0)); // Position, radius
-		//m_grids[0]->createRandom(0.0007);
-
-		m_grids[0]->createEdges(); // of the grid
-	#ifdef _DEBUG
-		auto na = m_grids[0]->numActiveVoxels();
-		auto totals = m_grids[0]->getNumberOfVoxels();
-		std::cout << "Number of active voxels " << na << " of " << totals << std::endl;
-	#endif
-		return m_grids[0];
-	}
+	std::shared_ptr<vxGrid> createGrid();
 	
-	std::shared_ptr<vxStorage::ImageProperties> prop() const
-	{
-		return m_prop;
-	}
+	std::shared_ptr<vxStorage::ImageProperties> prop() const;
 	
-	void setProp(const std::shared_ptr<vxStorage::ImageProperties> &prop)
-	{
-		m_prop = prop;
-	}
+	void setProp(const std::shared_ptr<vxStorage::ImageProperties> &prop);
 
-	
 	// devuelve 0 si no le da a la caja
 	// 1 si da y 2 y el resultado es optimo
 	int throwRay(const vxVector3d &ray, vxCollision &collide);
