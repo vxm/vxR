d611013526185b85a553421b17da0886bc0c261c
Mariano Blanc <marianobm@gmail.com>
2015-05-05 23:18:28 +0100

    removed code to draw circles, this now have his places with the new maps

--- vxShader.cpp
+++ vxShader.cpp
@@ -1,52 +1,16 @@
 #include "vxShader.h"
 #include <algorithm>
 
+
 namespace vxStorage {
 
-vxColor vxLambert::getColor(vxCollision &collide) const
+vxColor vxLambert::getColor(const vxCollision &collide) const
 {
-	auto distanceToCenter = 
-			vxVector2d(collide.getU()-.5,collide.getV()-.5).length();
-
 	double lumm = std::max(getLightLoop(collide), 0.0);
 
-	if(distanceToCenter<.075)
-	{
-		collide.setColor(lumm*2.0, lumm*2.0, lumm*2.0);
-	}
-	else if(distanceToCenter<.3)
-	{
-		collide.setColor(lumm*collide.getColor().getR(), 
-						 lumm*collide.getColor().getG(), 
-						 lumm*collide.getColor().getB());
-	}
-	else if(distanceToCenter<.31)
-	{
-		collide.setColor(lumm*2.0, lumm*2.0, lumm*2.0);
-	}
-	else if(distanceToCenter<.4)
-	{
-		collide.setColor(lumm*255.0, lumm*255.0, lumm*255.0);
-	}
-	else if(distanceToCenter<.41)
-	{
-		collide.setColor(lumm*2.0, lumm*2.0, lumm*2.0);
-	}	
-	else
-	{
-		constexpr const double margn = 0.01;
-		if ( (collide.getU()<margn || collide.getU()>(1.-margn)) 
-			 || (collide.getV()<margn || collide.getV()>(1.-margn)))
-		{
-			collide.setColor(lumm*2.0, lumm*3.0, lumm*29.0);
-		}
-		else
-		{
-			collide.setColor(lumm*255.0, lumm*255.0, lumm*155.0);
-		}
-	}
-
-	return collide.getColor();
+	vxColor col = m_map.getColor(collide);
+	
+	return col * lumm;
 }
 std::shared_ptr<vxLight> vxShader::light() const
 {
