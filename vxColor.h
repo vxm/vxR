67a064e1de3a190bb4a078ff44b639e8c2be32a8
Mariano Blanc <marianobm@gmail.com>
2015-05-05 23:58:31 +0100

    some int being returned cause a bad color conversion, members of other class of same type can be accessed like public, I guess can be done faster as well

--- vxColor.h
+++ vxColor.h
@@ -71,10 +71,10 @@ public:
 	
 	void reset() {m_r=0;m_g=0;m_b=0;m_a=1.0;}
 
-	void setR(int r) {m_r=r;}
-	void setG(int g) {m_g=g;}
-	void setB(int b) {m_b=b;}
-	void setA(int a) {m_a=a;}
+	void setR(double r) {m_r=r;}
+	void setG(double g) {m_g=g;}
+	void setB(double b) {m_b=b;}
+	void setA(double a) {m_a=a;}
 
 	vxColor get() const 
 		{return *this;}
@@ -82,10 +82,10 @@ public:
 	void get(double &ri, double &gi, double &bi, double &ai) const
 		{ri=m_r;gi=m_g;bi=m_b;ai=m_a;}
 
-	int getR() const {return m_r;}
-	int getG() const {return m_g;}
-	int getB() const {return m_b;}
-	int getA() const {return m_a;}
+	double getR() const {return m_r;}
+	double getG() const {return m_g;}
+	double getB() const {return m_b;}
+	double getA() const {return m_a;}
 
 	double lumma() const {return sqrt((double)m_r*m_r+m_g*m_g+m_b*m_b);}
 	void setToGamma(double gamma=2.2)
@@ -96,30 +96,35 @@ public:
 	}
 	
 	
-	vxColor operator+(const vxColor &other) const {return vxColor(other.getR()+m_r,other.getG()+m_g,other.getB()+m_b,other.getA()+m_a);}
+	vxColor operator+(const vxColor &other) const
+	{
+		return vxColor(other.m_r+m_r,
+					   other.m_g+m_g,
+					   other.m_b+m_b,
+					   other.m_a+m_a);
+	}
 	vxColor operator+(double factor) const {return vxColor(factor+m_r,factor+m_g,factor+m_b,factor+m_a);}
 	vxColor operator+(float factor) const {return vxColor(factor+m_r,factor+m_g,factor+m_b,factor+m_a);}
 	vxColor operator+(int factor) const {return vxColor(factor+m_r,factor+m_g,factor+m_b,factor+m_b);}
 	vxColor operator+(unsigned int factor) const {return vxColor(factor+m_r,factor+m_g,factor+m_b,factor+m_b);}
 
-	vxColor operator-(const vxColor &entrada) const {return vxColor(m_r-entrada.getR(),m_g-entrada.getG(),m_b-entrada.getB(),m_a-entrada.getA());}
+	vxColor operator-(const vxColor &entrada) const {return vxColor(m_r-entrada.m_r,m_g-entrada.m_g,m_b-entrada.m_b,m_a-entrada.m_a);}
 	vxColor operator-(double factor) const {return vxColor(m_r-factor,m_g-factor,m_b-factor,m_a-factor);}
 	vxColor operator-(float factor) const {return vxColor(m_r-factor,m_g-factor,m_b-factor,m_a-factor);}
 	vxColor operator-(int factor) const {return vxColor(m_r-factor,m_g-factor,m_b-factor,m_a-factor);}
 	vxColor operator-(unsigned int factor) const {return vxColor(m_r-factor,m_g-factor,m_b-factor,m_a-factor);}
 
-	vxColor operator*(const vxColor &entrada) const {return vxColor(entrada.getR()*m_r,entrada.getG()*m_g,entrada.getB()*m_b,entrada.getA()*m_a);}
+	vxColor operator*(const vxColor &entrada) const {return vxColor(entrada.m_r*m_r,entrada.m_g*m_g,entrada.m_b*m_b,entrada.m_a*m_a);}
 	vxColor operator*(double factor) const {return vxColor(factor*m_r,factor*m_g,factor*m_b,factor*m_a);}
 	vxColor operator*(float factor) const {return vxColor(factor*m_r,factor*m_g,factor*m_b,factor*m_a);}
 	vxColor operator*(int factor) const {return vxColor(factor*m_r,factor*m_g,factor*m_b,factor*m_a);}
 	vxColor operator*(unsigned int factor) const {return vxColor(factor*m_r,factor*m_g,factor*m_b,factor*m_a);}
 
-	vxColor operator/(const vxColor &entrada) const {return vxColor(entrada.getR()/(double)m_r,entrada.getG()/(double)m_g,entrada.getB()/(double)m_b,entrada.getA()/(double)m_a);}
+	vxColor operator/(const vxColor &entrada) const {return vxColor(entrada.m_r/(double)m_r,entrada.m_g/(double)m_g,entrada.m_b/(double)m_b,entrada.m_a/(double)m_a);}
 	vxColor operator/(double factor) const {return vxColor(m_r/(double)factor,m_g/(double)factor,m_b/(double)factor,m_a/(double)factor);}
 	vxColor operator/(float factor) const {return vxColor(m_r/(double)factor,m_g/(double)factor,m_b/(double)factor,m_a/(double)factor);}
 	vxColor operator/(int factor) const {return vxColor(m_r/double(factor),m_g/(double)factor,m_b/(double)factor,m_a/(double)factor);}
 	vxColor operator/(unsigned int factor) const {return vxColor(m_r/double(factor),m_g/(double)factor,m_b/(double)factor,m_a/(double)factor);}
-
 	
 	void toRGBA8888(unsigned char *tbuff) const
 	{
@@ -130,9 +135,7 @@ public:
 		tbuff++;
 
 		*tbuff = (unsigned char)char(m_b*255.0);
-		tbuff++;
 
-		*tbuff = (unsigned char)char(m_a*255.0);
 	}
 	
 	friend std::ostream& operator<<(std::ostream &os, const vxColor& c)
