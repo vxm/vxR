7f84a51b9961e710754207943eee886b5f23f225
Mariano Blanc <marianobm@gmail.com>
2015-06-02 09:02:51 +0100

    moved code to cpp

--- vxPLYImporter.cpp
+++ vxPLYImporter.cpp
@@ -5,3 +5,78 @@ using namespace vxCore;
 vxPLYImporter::vxPLYImporter()
 {
 }
+
+void vxPLYImporter::processPLYFile(const std::string &fileName)
+{
+	m_pointCloud.clear();
+	if (!FileUtils::fileExists(fileName))
+	{
+		std::cout << "PLY: File " << fileName << " doesn't exist" << std::endl;
+		return;
+	}
+
+	std::ifstream iFile(fileName);
+	std::string line;
+	unsigned int k {0};
+
+	// Not optional lines.
+	std::getline(iFile, line);
+	if(line!="ply")
+	{
+		std::cout << "PLY: File " << fileName << " doesn't contain ply info" << std::endl;
+		return;
+	}
+	
+	// checking if ASCII
+	std::getline(iFile, line);
+	if(line!="format ascii 1.0")
+	{
+		std::cout << "PLY: File " << fileName << " is not ASCII" << std::endl;
+		return;
+	}
+	
+	//optional lines
+	
+	// comments.
+	std::getline(iFile, line);
+	std::cout << "PLY File Comments:: '" << line.substr(8) << "'" << std::endl;
+	
+	// element vertex.
+	std::getline(iFile, line);
+	std::cout << "PLY: Num vertex " << line.substr(15) << std::endl;
+	int numVertex = strtol(line.substr(15).c_str(), 
+						   (char **)NULL, 
+						   10);
+	
+	do
+	{
+		std::getline(iFile, line);
+	}
+	while(line!="end_header");
+	
+	double x, y, z;
+	while(std::getline(iFile, line) && k<numVertex)
+	{
+		std::istringstream is(line);
+		
+		if(!(is >> x >> y >> z))
+		{
+			std::cout << "PLY: Line " << k << " could not be parsed as xyx double" << std::endl;
+		}
+		else
+		{
+			//std::cout << "PLY: X " << x << "  Y "  << y << "  Z "  << z << " found on line: " << k << std::endl;
+			m_pointCloud.push_back(vxVector3d(x,y,z));
+		}
+		
+		k++;
+	}
+	
+	std::cout << "PLY Ended:: '" << m_pointCloud.size() << "' points taken" << std::endl;
+	
+}
+
+std::vector<vxVector3d> &vxPLYImporter::getPointCloud()
+{
+	return m_pointCloud;
+}
--- vxPLYImporter.h
+++ vxPLYImporter.h
@@ -16,75 +16,9 @@ public:
 	
 	std::vector<vxVector3d> m_pointCloud;
 
-	void processPLYFile(const std::string &fileName)
-	{
-		if (!FileUtils::fileExists(fileName))
-		{
-			std::cout << "PLY: File " << fileName << " doesn't exist" << std::endl;
-			return;
-		}
-		m_pointCloud.clear();
-		
-		std::ifstream iFile(fileName);
-		std::string line;
-		unsigned int k {0};
-
-		std::getline(iFile, line);
-		if(line!="ply")
-		{
-			std::cout << "PLY: File " << fileName << " doesn't contain ply info" << std::endl;
-			return;
-		}
-		
-		// checking if ASCII
-		std::getline(iFile, line);
-		if(line!="format ascii 1.0")
-		{
-			std::cout << "PLY: File " << fileName << " is not ASCII" << std::endl;
-			return;
-		}
-		
-		// comments.
-		std::getline(iFile, line);
-		std::cout << "PLY File Comments:: '" << line.substr(8) << "'" << std::endl;
-		
-		// element vertex.
-		std::getline(iFile, line);
-		std::cout << "PLY: Num vertex " << line.substr(15) << std::endl;
-		int numVertex = strtol(line.substr(15).c_str(), 
-								 (char **)NULL, 
-								 10);
-
-		do
-		{
-			std::getline(iFile, line);
-		}
-		while(line!="end_header");
-		
-		double x, y, z;
-		while(std::getline(iFile, line) && k<numVertex)
-		{
-			std::istringstream is(line);
-			
-			if(!(is >> x >> y >> z))
-			{
-				std::cout << "PLY: Line " << k << " could not be parsed as xyx double" << std::endl;
-			}
-			else
-			{
-				//std::cout << "PLY: X " << x << "  Y "  << y << "  Z "  << z << " found on line: " << k << std::endl;
-				m_pointCloud.push_back(vxVector3d(x,y,z));
-			}
-		
-			k++;
-		}
-	}
+	void processPLYFile(const std::string &fileName);
 	
-	virtual std::vector<vxVector3d>& getPointCloud() override
-	{
-		return m_pointCloud;
-	}
-	
+	virtual std::vector<vxVector3d>& getPointCloud() override;
 };
 
 }
