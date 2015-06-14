a1fb14e3c2f428bba8e13cd5c53112d7d3ef4fe9
Mariano Blanc <marianobm@gmail.com>
2015-06-14 23:48:33 +0100

    new class ititialised from well known ramp, needs rework

--- vxBitMap2d.cpp
+++ vxBitMap2d.cpp
@@ -1,5 +1,54 @@
 #include "vxBitMap2d.h"
 
+using namespace vxCore;
+
 vxBitMap2d::vxBitMap2d()
 {
 }
+
+vxColor vxBitMap2d::compute(const vxCollision &collision) const
+{
+	vxColor outColor;
+
+	auto distanceToCenter = 
+			vxVector2d(collision.u()-.5,collision.v()-.5).length();
+
+	outColor.set(vxColor::black);
+
+	if(m_circle1)
+	{
+		if(distanceToCenter>m_radius[0] && distanceToCenter<m_radius[1])
+		{
+			outColor.set(vxColor::purple);
+		}
+	}
+
+	if(m_circle2)
+	{
+		if(distanceToCenter>m_radius[1] && distanceToCenter<m_radius[2])
+		{
+			outColor.set(vxColor::black);
+		}
+	}
+	
+	if(m_circle3)
+	{
+		if(distanceToCenter>m_radius[2] && distanceToCenter<m_radius[3])
+		{
+			outColor.set(vxColor::blue);
+		}
+	}
+	
+	if(m_margin)
+	{
+		if (((collision.u()<m_margn || collision.u()>(1.-m_margn)) 
+				|| (collision.v()<m_margn || collision.v()>(1.-m_margn))))
+		{
+			outColor.set(vxColor::black);
+		}
+	}
+	
+	return outColor;
+}
+
+
--- vxBitMap2d.h
+++ vxBitMap2d.h
@@ -1,10 +1,30 @@
 #ifndef VXBITMAP2D_H
 #define VXBITMAP2D_H
+#include "vxMap2d.h"
+
+namespace vxCore
+{
 
 class vxBitMap2d : public vxMap2d
 {
-public:
-	vxBitMap2d();
+	public:
+		vxBitMap2d();
+		
+		// vxMap interface
+	public:
+		virtual vxColor compute(const vxCollision &collision) const;
+
+	double m_radius[5] = {0.1, 0.2, 0.3, 0.4, 0.5};
+		
+	bool m_circle1 {true};
+	bool m_circle2 {true};
+	bool m_circle3 {true};
+	bool m_margin {true};
+
+	double m_margn = 0.055;
 };
+
+
+}
 
 #endif // VXBITMAP2D_H
