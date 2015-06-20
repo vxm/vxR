b31f7135f464440fb4bca7bd1d89a394edb29d92
Mariano Blanc <marianobm@gmail.com>
2015-06-20 10:54:13 +0100

    Added legoblock geometry

---
+++ vxLegoBlock.cpp
@@ -0,0 +1,5 @@
+#include "vxLegoBlock.h"
+
+vxLegoBlock::vxLegoBlock()
+{
+}
---
+++ vxLegoBlock.h
@@ -0,0 +1,10 @@
+#ifndef VXLEGOBLOCK_H
+#define VXLEGOBLOCK_H
+
+class vxLegoBlock : public vxBoxN
+{
+public:
+	vxLegoBlock();
+};
+
+#endif // VXLEGOBLOCK_H
