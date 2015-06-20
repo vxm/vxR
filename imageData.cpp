6e15e50ca1969fd68b77e46d63405372c5a1ea0e
Mariano Blanc <marianobm@gmail.com>
2015-06-20 20:48:46 +0100

    Finished basic functioning. It works well now

--- imageData.cpp
+++ imageData.cpp
@@ -7,17 +7,42 @@ ImageData::ImageData(std::shared_ptr<ImageProperties> &prop)
 	m_prop = prop;
 }
 
+ImageData::ImageData()
+{
+	
+}
+
+void ImageData::setProperties(std::shared_ptr<ImageProperties> &prop)
+{
+	m_prop = prop;
+}
+
+unsigned char *ImageData::get(const vxVector2d &coords) const
+{
+	size_t numElements = m_prop->numElements();
+	const unsigned int&& compX = coords.y() * (m_prop->rx()+1);
+	const unsigned int&& compY = coords.x() * (m_prop->ry()+1);
+	unsigned int dist = (compX + (compY * m_prop->rx())) * m_prop->numChannels();
+	//std::cout << (m_prop->rx() * m_prop->ry() * m_prop->numChannels()) << "         " << dist << std::endl; 
+	if(dist>numElements)
+	{
+		dist = numElements;
+	}
+	
+	return m_pc.get() + dist;
+}
+
 unsigned char* ImageData::initialise()
 {
 	static_assert(sizeof(float)==4, "double is no 32bits");
 	static_assert(sizeof(double)==8, "double is no 64bits");
 	static_assert(sizeof(unsigned char)==1, "unsigned char is no 8bits");
-
+	
 	size_t numElements = m_prop->numElements();
 	switch(m_prop->format())
 	{
-		case ImageProperties::ImgFormat::k8:
-			m_pc.reset(new unsigned char[numElements]);
+	case ImageProperties::ImgFormat::k8:
+		m_pc.reset(new unsigned char[numElements]);
 		break;
 		case ImageProperties::ImgFormat::k32:
 			m_pf.reset(new float[numElements]);
@@ -39,4 +64,31 @@ unsigned char* ImageData::initialise()
 	}		
 	
 	return buff;
+}
+
+void ImageData::copyData(const unsigned char *iBuff)
+{
+	size_t nElements = m_prop->numElements();
+	
+	unsigned char *buff = m_pc.get();
+	unsigned char *tmpp = buff;	
+	unsigned char *iTmpp = const_cast<unsigned char *>(iBuff);
+	while(tmpp<buff+nElements)
+	{
+		*tmpp = *iTmpp;
+		tmpp++;
+		iTmpp++;
+
+		*tmpp = *iTmpp;
+		tmpp++;
+		iTmpp++;
+
+		*tmpp = *iTmpp;
+		tmpp++;
+		iTmpp++;
+
+		*tmpp = *iTmpp;
+		tmpp++;
+		iTmpp++;
+	}
 }
--- imageData.h
+++ imageData.h
@@ -1,6 +1,8 @@
 #ifndef IMAGEDATA_H
 #define IMAGEDATA_H
 #include "memory"
+
+#include "vxVector.h"
 #include "ImageProperties.h"
 
 namespace vxCore {
@@ -9,13 +11,20 @@ class ImageData
 {
 public:
 	ImageData(std::shared_ptr<ImageProperties> &prop);
+	ImageData();
+	
+	void setProperties(std::shared_ptr<ImageProperties> &prop);
 	std::shared_ptr<ImageProperties> m_prop;
 
 	std::unique_ptr <unsigned char[]>	m_pc = nullptr;
 	std::unique_ptr <double[]>			m_pd = nullptr;
 	std::unique_ptr <float[]>			m_pf = nullptr;
 
+	unsigned char *get(const vxVector2d &coords) const;
+
 	unsigned char *initialise();
+	
+	void copyData(const unsigned char *iBuff);
 };
 
 }
