8de5d79b4bb041007bb0f77b6815cb5f8a7508d1
Mariano Blanc <marianobm@gmail.com>
2015-07-28 01:55:11 +0100

    Added math unit tests, starting with rectAndXPlane

--- MathUtilsUnitTest.h
+++ MathUtilsUnitTest.h
@@ -1,10 +1,20 @@
 #ifndef MATHUTILSUNITTEST_H
 #define MATHUTILSUNITTEST_H
 
-class MathUtilsUnitTest
+#include <string>
+
+#include "vxUnitTest.h"
+#include "MathUtils.h"
+
+namespace vxCore {
+
+class MathUtilsUnitTest : public vxUnitTest
 {
 public:
 	MathUtilsUnitTest();
+	
+	int testFunction_rectAndXPlane();
+	static int testMathUtils();
 };
-
+}
 #endif // MATHUTILSUNITTEST_H
