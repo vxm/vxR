3ad924c746d2a806fc7c8b0946cda111f89a2b4a
Mariano Blanc <marianobm@gmail.com>
2015-08-03 07:55:53 +0100

    changed preferences for texture

--- vxCirclesMap.h
+++ vxCirclesMap.h
@@ -14,14 +14,14 @@ class vxCirclesMap : public vxMap2d
 	public:
 		virtual vxColor compute(const vxCollision &collision) const;
 
-	double m_radius[5] = {0.05, 0.08, 0.1, 0.15, 0.5};
+	double m_radius[5] = {0.05, 0.1, 0.2, 0.25, 0.5};
 		
 	bool m_circle1 {true};
 	bool m_circle2 {true};
 	bool m_circle3 {true};
 	bool m_margin  {true};
 
-	double m_margn = 0.04;
+	double m_margn = 0.02;
 };
 
 
