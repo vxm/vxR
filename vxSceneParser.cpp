c04b51f3272568c3ed8980b4625594860b853a30
Mariano Blanc <marianobm@gmail.com>
2015-08-17 16:57:14 +0100

    Scene Parser reads scene file and code is prepared to be used. Very beginning for this function. WIP
    Signed-off-by: Mariano Blanc <marianobm@gmail.com>

--- vxSceneParser.cpp
+++ vxSceneParser.cpp
@@ -1,5 +1,59 @@
 #include "vxSceneParser.h"
 
-vxSceneParser::vxSceneParser()
+using namespace vxCore;
+
+vxSceneParser::vxSceneParser(const std::string &fileName)
+	:m_fileName(fileName)
 {
+}
+
+decltype(auto) vxSceneParser::getLine(std::ifstream &f, std::string &line) const
+{
+	decltype(auto) a = std::getline(f, line);
+	
+//	while(line[0])
+	{
+		std::getline(f, line);
+	}
+	
+	return a;
+}
+
+void vxSceneParser::procesScene()
+{
+	if (!FileUtils::fileExists(m_fileName))
+	{
+		std::cout << "Scene file " << m_fileName << " doesn't exist" << std::endl;
+		return;
+	}
+
+	std::ifstream iFile(m_fileName);
+	std::string line;
+	unsigned int k {0};
+
+	// Not optional lines.
+	getLine(iFile, line);
+	
+	// checking this contains a scene
+	if(line!="#vxR scene")
+	{
+		std::cout << "scene parse: '" << m_fileName << "' doesn't contain vxR scene information." << std::endl;
+		return;
+	}
+	
+	// checking if ASCII
+	std::getline(iFile, line);
+	if(line!="#format ascii 1.0")
+	{
+		std::cout << "scene parse: File " << m_fileName << " is not ASCII" << std::endl;
+		return;
+	}
+	
+	// capturing nodes.
+	while(getLine(iFile, line))
+	{	
+		std::cout << "line: '" << line << "'" << std::endl;
+	}
+
+	std::cout << "scene parse: " << m_fileName << " finished:: '" << m_nodes.size() << "' nodes taken" << std::endl;
 }
--- vxSceneParser.h
+++ vxSceneParser.h
@@ -1,10 +1,69 @@
 #ifndef VXSCENEPARSER_H
 #define VXSCENEPARSER_H
 
-class vxSceneParser
+#include <functional>
+#include "FileUtils.h"
+#include "vxMatrix.h"
+
+namespace vxCore {
+
+static long nNodes{0};
+
+struct vxNode
 {
-public:
-	vxSceneParser();
+	vxNode()
+	{
+		id=nNodes++;
+	}
+	
+	std::string name;
+	std::string type;
+	vxMatrix transform{vxMatrix::identity};
+	
+	int id{0};
+
+	friend std::ostream& operator<<(std::ostream &os, const vxNode& v)
+	{
+		return os << "Node ::" << v.id << ":: " << v.name << std::endl
+					 << "Type: " << v.type << std::endl;
+	}
+
 };
 
+class nodeMap:public std::map<std::string, vxNode*>
+{
+public:
+	nodeMap()
+	{}
+	
+};
+
+class vxSceneParser
+{
+	std::string m_fileName;
+	nodeMap m_nodes;
+	
+public:
+
+	vxSceneParser(const std::string &fileName);
+	
+	void procesScene();
+	
+	decltype(auto) getLine(std::ifstream &f, std::string &line) const;
+
+	void printSceneInfo()
+	{
+		for(const auto& node: m_nodes)
+		{
+			std::cout << (*node.second) << std::endl;
+		}
+	}
+	
+	void clear()
+	{
+		m_nodes.clear();
+	}
+};
+
+}
 #endif // VXSCENEPARSER_H
