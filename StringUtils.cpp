671798d76e0d83c0a5bcf9a9e6f42d704b245dfa
Mariano Blanc <marianobm@gmail.com>
2015-09-17 19:30:22 +0100

    Added indentation search to string utils.

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -3,3 +3,31 @@
 StringUtils::StringUtils()
 {
 }
+
+unsigned int StringUtils::indentation(const std::string &line)
+{
+	auto n = 0u;
+
+	for(auto c:line)
+	{
+		auto notFound{true};
+		if(c=='\t')
+		{
+			n+=SPACES_ARE_A_TAB;
+			notFound = false;
+		}
+		
+		if(std::isspace(c))
+		{
+			n++;
+			notFound = false;
+		}
+		
+		if(notFound)
+		{
+			break;
+		}
+	}
+
+	return n;
+}
--- StringUtils.h
+++ StringUtils.h
@@ -1,10 +1,19 @@
 #ifndef STRINGUTILS_H
 #define STRINGUTILS_H
 
+#include <string>
+#include <iostream>
+
+#define SPACES_ARE_A_TAB 4
+
 class StringUtils
 {
 public:
 	StringUtils();
+
+	//Returns indentation of a line, or in other words the tabs or
+	// int division of SPACES_ARE_A_TAB in spaces or a combination of both.
+	static unsigned int indentation(const std::string &line);
 };
 
 #endif // STRINGUTILS_H
