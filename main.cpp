5ddfae3d475b7b7df24a11121b7f9dd1b315e89a
Mariano Blanc <marianobm@gmail.com>
2015-10-01 19:18:49 +0100

    Obtaining render properties from render.

--- main.cpp
+++ main.cpp
@@ -53,58 +53,66 @@ int executeRenderProcess(int argc, char *argv[])
 	auto sceneParser = std::make_shared<vxSceneParser>(scenePath);
 	sceneParser->procesScene();
 
-	// Img properties for render.
-	auto imgDesc = std::make_shared<ImageProperties>(700,700);
-	
-	// create the render process
-	render rp(imgDesc);
-	
-	rp.setDatabase(sceneParser);
-	
-	// creates the bucket list (vector) using the img 
-	// description.
-	rp.createBucketList();
-	
-	std::cout << "Render process: " << TimeUtils::decorateTime(start,2) << std::endl;
-	
-	// executes the render.
-	if(rp.execute()==vxStatus::code::kError)
+	for(auto renderPropertiesNode: sceneParser->getNodesByType("vxRenderSettings"))
 	{
-		return 1;
-	}
-	
-	std::cout << "Finished render : " 
-			<< TimeUtils::decorateTime(start,2) << std::endl;
-
-	// generates an image buffer and fills it 
-	// with the render results. Buffer properties
-	// are based on ImageProperties stored in the 
-	// render process object.
-	auto bff = rp.generateImage();
-
-	std::cout << "Ended creation of the image: " 
-			<< TimeUtils::decorateTime(start,2) << std::endl;
-
-	// storing an image from the buffer obtained.
-	if (bff!=nullptr)
-	{
-		QImage img( bff,
-					imgDesc->rx(),
-					imgDesc->ry(),
-					QImage::Format_RGBA8888);
-
-		std::string fileName = 
-				FileUtils::makeUnique(baseName);
-
-		//TODO: investigate other formats and resolution limits
-		img.save(QString(fileName.c_str()),"TIFF",100);
+		const auto resolution = renderPropertiesNode->getVector2dAttribute("resolution");
+		const auto pixelSamples = renderPropertiesNode->getIntAttribute("pixelSamples");
+		const auto reflectionSamples = renderPropertiesNode->getIntAttribute("reflectionSamples");
 		
-		std::cout << "File saved : " 
-					<< fileName 
-					<< std::endl;
+		// Img properties for render.
+		auto renderProperties = std::make_shared<ImageProperties>(resolution.x(),
+																  resolution.y());
+		// create the render process
+		render rp(renderProperties);
+		rp.setDatabase(sceneParser);
+		rp.setVisSamples(pixelSamples);
+		rp.setReflectionSamples(reflectionSamples);
 		
-		std::cout << "Finished image generation : " 
+		// creates the bucket list (vector) using the img 
+		// description.
+		rp.createBucketList();
+		
+		std::cout << "Render process: " << TimeUtils::decorateTime(start,2) << std::endl;
+		
+		// executes the render.
+		if(rp.execute()==vxStatus::code::kError)
+		{
+			return 1;
+		}
+		
+		std::cout << "Finished render : " 
 				<< TimeUtils::decorateTime(start,2) << std::endl;
+
+		// generates an image buffer and fills it 
+		// with the render results. Buffer properties
+		// are based on ImageProperties stored in the 
+		// render process object.
+		auto bff = rp.generateImage();
+	
+		std::cout << "Ended creation of the image: " 
+				<< TimeUtils::decorateTime(start,2) << std::endl;
+	
+		// storing an image from the buffer obtained.
+		if (bff!=nullptr)
+		{
+			QImage img( bff,
+						renderProperties->rx(),
+						renderProperties->ry(),
+						QImage::Format_RGBA8888);
+	
+			std::string fileName = 
+					FileUtils::makeUnique(baseName);
+	
+			//TODO: investigate other formats and resolution limits
+			img.save(QString(fileName.c_str()),"TIFF",100);
+			
+			std::cout << "File saved : " 
+						<< fileName 
+						<< std::endl;
+			
+			std::cout << "Finished image generation : " 
+					<< TimeUtils::decorateTime(start,2) << std::endl;
+		}
 	}
 	//return a.exec();
 	std::cout << "done!" << std::endl;
