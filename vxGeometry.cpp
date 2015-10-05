ccc4807a0fc29bbc2313bf2862a743b45b2c3d2d
Mariano Blanc <marianobm@gmail.com>
2015-10-05 08:08:25 +0100

    Added getter and setters in Geometry

--- vxGeometry.cpp
+++ vxGeometry.cpp
@@ -1,7 +1,24 @@
 #include "vxGeometry.h"
 
+using namespace vxCore;
+
 vxGeometry::vxGeometry()
 {
 	
+}
+
+void vxGeometry::addVertex(const vxVector3d &v3)
+{
+	m_vertices.push_back(v3);
+}
+
+unsigned int vxGeometry::vertexCount() const
+{
+	return m_vertices.size();
+}
+
+unsigned int vxGeometry::triangleCount() const
+{
+	return m_triangles.size();
 }
 
--- vxGeometry.h
+++ vxGeometry.h
@@ -1,15 +1,49 @@
 #ifndef VXGEOMETRY_H
 #define VXGEOMETRY_H
 
+#include <vector>
+#include "vxVector.h"
+
+namespace vxCore {
+
+
+struct int3
+{
+	int3(unsigned int ia,
+		 unsigned int ib,
+		 unsigned int ic)
+		:a{ia}
+		,b{ib}
+		,c{ic}
+	{
+	}
+	
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+};
 
 class vxGeometry
 {
 public:
+
 	vxGeometry();
-	
-signals:
-	
-public slots:
+
+	std::vector<vxVector3d> m_vertices;
+	std::vector<int3> m_triangles;
+	std::vector<vxVector3d> m_normals;
+
+	void addVertex(const vxVector3d &v3);
+	void addTriangle(unsigned int a,
+					 unsigned int b,
+					 unsigned int c)
+	{
+		m_triangles.push_back(int3(a,b,c));
+	}
+
+	unsigned int vertexCount() const;
+	unsigned int triangleCount() const;
 };
 
+}
 #endif // VXGEOMETRY_H
