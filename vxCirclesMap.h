312df661252c3ca1419c96ed9af0f4f3880c5a4f
Mariano Blanc <marianobm@gmail.com>
2015-10-11 13:54:59 +0100

    changed compute(const vxCollision &collision) function to be const parameter

--- vxCirclesMap.h
+++ vxCirclesMap.h
@@ -7,12 +7,12 @@ namespace vxCore
 
 class vxCirclesMap : public vxMap2d
 {
-	public:
-		vxCirclesMap();
-		
-		// vxMap interface
-	public:
-		virtual vxColor compute(const vxCollision &collision) const;
+public:
+	vxCirclesMap();
+	
+	// vxMap interface
+public:
+	virtual vxColor compute(const vxCollision &collision) const;
 
 	double m_radius[5] = {0.05, 0.2, 0.32, 0.45, 0.5};
 		
@@ -21,7 +21,7 @@ class vxCirclesMap : public vxMap2d
 	bool m_circle3 {false};
 	bool m_margin  {true};
 
-	double m_margn = 0.05;
+	double m_margn = 0.04;
 };
 
 
