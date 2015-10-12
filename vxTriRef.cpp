dfb1da8a1cad6a451918a7426ba8bfd9fc4b24ca
Mariano Blanc <marianobm@gmail.com>
2015-10-12 09:21:56 +0100

    Populated triRef with const ref values and getNormal with cache

--- vxTriRef.cpp
+++ vxTriRef.cpp
@@ -1,5 +1,24 @@
 #include "vxTriRef.h"
 
-vxTriRef::vxTriRef()
+using namespace vxCore;
+
+vxCore::vxTriRef::vxTriRef(const vxVector3d &a, 
+						   const vxVector3d &b, 
+						   const vxVector3d &c)
+	:p1(a)
+	,p2(b)
+	,p3(c)
 {
+	
+}
+
+vxVector3d vxTriRef::getNormal() 
+{
+	if(n)
+	{
+		return *n;
+	}
+	
+	n = std::make_unique<vxVector3d>((p2-p1).cross(p3-p1));
+	return *n;
 }
--- vxTriRef.h
+++ vxTriRef.h
@@ -1,10 +1,26 @@
 #ifndef VXTRIREF_H
 #define VXTRIREF_H
+#include <memory>
+#include "vxVector.h"
+
+namespace vxCore
+{
 
 class vxTriRef
 {
+	
 public:
-	vxTriRef();
+	vxTriRef(const vxVector3d& a,
+			 const vxVector3d& b,
+			 const vxVector3d& c);
+
+	const vxVector3d &p1;
+	const vxVector3d &p2;
+	const vxVector3d &p3;
+	std::unique_ptr<vxVector3d> n;
+	
+	vxVector3d getNormal();
 };
 
+}
 #endif // VXTRIREF_H
