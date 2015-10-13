51be67982bbca6d237e358c0aab0d4af49759438
Mariano Blanc <marianobm@gmail.com>
2015-10-13 18:59:46 +0100

    Added three points interface for free planes

--- vxPlane.h
+++ vxPlane.h
@@ -3,6 +3,7 @@
 
 #include "vxCollision.h"
 #include "vxRay.h"
+#include "vxTriRef.h"
 
 namespace vxCore {
 
@@ -12,6 +13,10 @@ class vxPlane
 	double m_y {0.0};
 	double m_z {0.0};
 	double m_d {0.0};
+	
+	vxVector3d m_point1;
+	vxVector3d m_point2;
+	vxVector3d m_point3;
 	
 	vxColor m_color{vxColor::white};
 	
@@ -69,6 +74,12 @@ class vxPlane
 	
 	vxColor color() const;
 	void setColor(const vxColor &color);
+	vxVector3d point1() const;
+	void setPoint1(const vxVector3d &point1);
+	vxVector3d point2() const;
+	void setPoint2(const vxVector3d &point2);
+	vxVector3d point3() const;
+	void setPoint3(const vxVector3d &point3);
 };
 
 }
