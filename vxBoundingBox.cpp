0d6dd9f0af8df7d38b0a72ac1545fbc3fb2a3d20
Mariano Blanc <marianobm@gmail.com>
2015-10-24 16:44:12 +0100

    Initialized bb with basic operations

--- vxBoundingBox.cpp
+++ vxBoundingBox.cpp
@@ -1,5 +1,125 @@
 #include "vxBoundingBox.h"
 
+using namespace vxCore;
+
 vxBoundingBox::vxBoundingBox()
 {
+}
+
+void vxBoundingBox::extend(const vxVector3d &limit)
+{
+	if(!m_init)
+	{
+		m_maxx = m_minx = limit.x();
+		m_maxy = m_miny = limit.y();
+		m_maxz = m_minz = limit.z();
+		m_init = true;
+		return;
+	}
+	
+	m_minx = std::min(limit.x(), m_minx);
+	m_miny = std::min(limit.y(), m_miny);
+	m_minz = std::min(limit.z(), m_minz);
+	
+	m_maxx = std::max(limit.x(), m_maxx);
+	m_maxy = std::max(limit.y(), m_maxy);
+	m_maxz = std::max(limit.z(), m_maxz);
+}
+
+double vxBoundingBox::minx() const
+{
+	return m_minx;
+}
+
+void vxBoundingBox::setMinx(double minx)
+{
+	m_minx = minx;
+}
+
+double vxBoundingBox::miny() const
+{
+	return m_miny;
+}
+
+void vxBoundingBox::setMiny(double miny)
+{
+	m_miny = miny;
+}
+
+double vxBoundingBox::minz() const
+{
+	return m_minz;
+}
+
+void vxBoundingBox::setMinz(double minz)
+{
+	m_minz = minz;
+}
+
+double vxBoundingBox::maxx() const
+{
+	return m_maxx;
+}
+
+void vxBoundingBox::setMaxx(double maxx)
+{
+	m_maxx = maxx;
+}
+
+double vxBoundingBox::maxy() const
+{
+	return m_maxy;
+}
+
+void vxBoundingBox::setMaxy(double maxy)
+{
+	m_maxy = maxy;
+}
+
+double vxBoundingBox::maxz() const
+{
+	return m_maxz;
+}
+
+void vxBoundingBox::setMaxz(double maxz)
+{
+	m_maxz = maxz;
+}
+
+bool vxBoundingBox::contains(const vxVector3d &v) const
+{
+	return v.x() >= m_minx
+			&&  v.y() >= m_miny
+			&&  v.z() >= m_minz
+			&&  v.x() <= m_maxx
+			&&  v.y() <= m_maxy
+			&&  v.z() <= m_maxz;
+}
+
+bool vxBoundingBox::hasCollision(const vxRay &ray) const
+{
+	if(contains(ray.origin()))
+	{
+		return true;
+	}
+	
+	const auto&& hitX = MU::rayAndXPlane(ray, std::signbit(m_maxx) ? m_maxx : m_minx);
+	if( hitX.z()<=m_maxz && hitX.z()>=m_minz && hitX.y()<=m_maxy && hitX.y()>=m_miny)
+	{
+		return true;
+	}
+	
+	const auto hitY = MU::rayAndYPlane(ray, std::signbit(m_maxy) ? m_maxy : m_miny);
+	if( hitY.x()<=m_maxx && hitY.x()>=m_minx && hitY.z()<=m_maxz && hitY.z()>=m_minz)
+	{
+		return true;
+	}
+	
+	const auto hitZ = MU::rayAndZPlane(ray, std::signbit(m_maxz) ? m_maxz : m_minz);
+	if( hitZ.x()<=m_maxx && hitZ.x()>=m_minx && hitZ.y()<=m_maxy && hitZ.y()>=m_miny)
+	{
+		return true;
+	}
+	
+	return false;
 }
--- vxBoundingBox.h
+++ vxBoundingBox.h
@@ -1,10 +1,52 @@
 #ifndef VXBOUNDINGBOX_H
 #define VXBOUNDINGBOX_H
 
+#include <atomic>
+#include "vxVector.h"
+#include "vxRay.h"
+#include "vxCollision.h"
+#include "MathUtils.h"
+
+namespace vxCore {
+
+
 class vxBoundingBox
 {
+	std::atomic_bool m_init{false};
+	
+	double m_minx;
+	double m_miny;
+	double m_minz;
+
+	double m_maxx;
+	double m_maxy;
+	double m_maxz;
+
 public:
 	vxBoundingBox();
+	
+	void extend(const vxVector3d &limit);
+	double minx() const;
+	void setMinx(double minx);
+	double miny() const;
+	void setMiny(double miny);
+	double minz() const;
+	void setMinz(double minz);
+	double maxx() const;
+	void setMaxx(double maxx);
+	double maxy() const;
+	void setMaxy(double maxy);
+	double maxz() const;
+	void setMaxz(double maxz);
+
+	bool contains(const vxVector3d &v) const;
+	
+	
+	//renderable interface
+	bool throwRay(const vxRay &ray) const;
+	int throwRay(const vxRay &ray, vxCollision &collide) const;
+	bool hasCollision(const vxRay &ray) const;
 };
 
+}
 #endif // VXBOUNDINGBOX_H
