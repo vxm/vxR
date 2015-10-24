a9ce67e6e16a5aa742c84f52e436cfd71e011706
Mariano Blanc <marianobm@gmail.com>
2015-10-24 16:42:52 +0100

    added BoundingBox class

---
+++ vxBoundingBox.cpp
@@ -0,0 +1,5 @@
+#include "vxBoundingBox.h"
+
+vxBoundingBox::vxBoundingBox()
+{
+}
---
+++ vxBoundingBox.h
@@ -0,0 +1,10 @@
+#ifndef VXBOUNDINGBOX_H
+#define VXBOUNDINGBOX_H
+
+class vxBoundingBox
+{
+public:
+	vxBoundingBox();
+};
+
+#endif // VXBOUNDINGBOX_H
