c9cb42d61e51bb4e64f304ac55b21bf29a75cf73
Mariano Blanc <marianobm@gmail.com>
2015-11-27 08:57:43 +0000

    Definition of the different raytrace methods for the BroadPhase

--- vxBroadPhase.cpp
+++ vxBroadPhase.cpp
@@ -4,21 +4,56 @@ using namespace vxCore;
 
 vxBroadPhase::vxBroadPhase()
 {
-	
+	m_root = std::make_shared<vxBoundingBox>();
 }
 
-bool vxBroadPhase::throwRay(const vxRay &) const
+void vxBroadPhase::addGeometry(std::shared_ptr<vxGeometry> geo)
 {
+	m_root->extend(*geo->boundingBox());
+	m_geometries.push_back(geo);
+}
+
+bool vxBroadPhase::throwRay(const vxRay &ray) const
+{
+	for(auto&& geo:m_geometries)
+	{
+		if(geo->throwRay(ray))
+		{
+			return true;
+		}
+	}
 	
+	return false;
 }
 
 int vxBroadPhase::throwRay(const vxRay &ray, vxCollision &collide) const
 {
+//	if(!m_root->hasCollision(ray))
+//	{
+//		return 0;
+//	}
 	
+	for(auto&& geo:m_geometries)
+	{
+		if(geo->throwRay(ray, collide))
+		{
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
-bool vxBroadPhase::hasCollision(const vxRay &) const
+bool vxBroadPhase::hasCollision(const vxRay &ray) const
 {
+	for(auto&& geo:m_geometries)
+	{
+		if(geo->hasCollision(ray))
+		{
+			return true;
+		}
+	}
 	
+	return false;
 }
 
