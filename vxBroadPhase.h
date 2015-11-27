8bec0ea860a50dc536ae3d4e10a0d76f0edeea67
Mariano Blanc <marianobm@gmail.com>
2015-11-27 09:00:05 +0000

    Added vector of refs to geometries, added root bb and tried to use it as the first visibility cull criteria, but makes things slower!!

--- vxBroadPhase.h
+++ vxBroadPhase.h
@@ -3,19 +3,27 @@
 #include "vxRay.h"
 #include "vxCollision.h"
 #include "vxVector.h"
+#include "vxGeometry.h"
 
 namespace vxCore {
 
 
 class vxBroadPhase
 {
+	
+	std::vector<std::shared_ptr<vxGeometry>> m_geometries;
+	
+	std::shared_ptr<vxBoundingBox> m_root;
+	
 public:
 	vxBroadPhase();
 
+	void addGeometry(std::shared_ptr<vxGeometry> geo);
+	
 	//renderable interface
-	bool throwRay(const vxRay &) const;
+	bool throwRay(const vxRay &ray) const;
 	int throwRay(const vxRay &ray, vxCollision &collide) const;
-	bool hasCollision(const vxRay &) const;
+	bool hasCollision(const vxRay &ray) const;
 };
 
 }
