bc36a29e543a27aed4e620c0d0ba40632e117b58
Mariano Blanc <marianobm@gmail.com>
2016-01-15 08:54:19 +0000

    BoundingBox is now a grid, WIP, locates new geometries, but still can't see them.

--- vxBroadPhase.cpp
+++ vxBroadPhase.cpp
@@ -1,16 +1,17 @@
 #include "vxBroadPhase.h"
 #include <limits>
 
+#define DRAWBBOX 1
+
 using namespace vxCore;
 
 vxBroadPhase::vxBroadPhase()
 {
-	m_root = std::make_shared<vxBoundingBox>();
+	m_bb = std::make_shared<vxBoundingBox>();
 }
 
 void vxBroadPhase::addGeometry(vxGeometryHandle geo)
 {
-	m_root->extend(*geo->boundingBox());
 	m_geometries.push_back(geo);
 }
 
@@ -34,7 +35,128 @@ vxBoundingBoxHandle vxBroadPhase::closestBox(const v3 &p) const
 	return ret;
 }
 
+void vxBroadPhase::updateCache()
+{
+	const auto dngs = m_geometries.size() * 2;
 
+	m_xvalues.resize(dngs);
+	m_yvalues.resize(dngs);
+	m_zvalues.resize(dngs);
+
+	//run through geometries
+	unsigned int i=0;
+	unsigned int ii=1;
+	
+	m_c_size = m_rx * m_ry * m_rz;
+	
+	m_bb->clear();
+	m_members.resize(m_c_size);
+
+	for(auto geo:m_geometries)
+	{
+		auto bb = geo->boundingBox();
+		
+		m_xvalues[i]= bb->minX();
+		m_xvalues[ii]= bb->maxX();
+		
+		m_yvalues[i]= bb->minY();
+		m_yvalues[ii]= bb->maxY();
+		
+		m_zvalues[i]= bb->minZ();
+		m_zvalues[ii]= bb->maxZ();
+		
+		m_bb->extend(*bb);
+		
+		++ii;
+		++i;
+	}
+	
+	for(auto geo:m_geometries)
+	{
+		locateAndRegister(geo);
+	}
+	
+	m_rz = m_ry = m_rx = dngs-1;
+}
+
+unsigned long vxBroadPhase::index(unsigned int a, unsigned int b, unsigned int c) const
+{
+	return ((m_ry * m_rx) * c) + (m_rx * b) + a;
+}
+
+unsigned long vxBroadPhase::lookupVoxel(const v3 &v, 
+										int &a, 
+										int &b, 
+										int &c) const
+{
+	a = 0;
+	for(unsigned int i=1;i<m_xvalues.size()-1;i++)
+	{
+		if( v.x() < m_xvalues[i] )
+		{
+			break;
+		}
+
+		a++;
+	}
+
+	b = 0;
+	for(unsigned int i=1;i<m_yvalues.size()-1;i++)
+	{
+		if( v.y() < m_yvalues[i] )
+		{
+			break;
+		}
+
+		b++;
+	}
+	
+	c = 0;
+	for(unsigned int i=1;i<m_zvalues.size()-1;i++)
+	{
+		if( v.z() < m_zvalues[i] )
+		{
+			break;
+		}
+
+		c++;
+	}
+	
+	if(a<0 || b<0 || c<0)
+	{
+		std::cerr << "index out of bounds in GeoGrid " << __LINE__ << std::endl;
+	}
+	
+	return index(a,b,c);
+}
+
+void vxBroadPhase::locateAndRegister(vxGeometryHandle geo)
+{
+	auto bb = geo->boundingBox();
+	auto cnt = bb->center();
+	
+	int a,b,c;
+	auto idx = lookupVoxel(cnt, a, b, c);
+	
+	if(indexIsValid(idx))
+	{
+		if(m_members[idx].geoRef == nullptr)
+		{
+			m_members[idx].geoRef = geo;
+			m_members[idx].index = idx;
+		}
+		else
+		{
+			std::cerr << "Adding geometry to a occupied voxel (unhandled)" 
+					  << std::endl;
+		}
+	}
+}
+
+bool vxBroadPhase::indexIsValid(const long idx) const
+{
+	return !(idx<0l || idx>=m_c_size);
+}
 
 bool vxBroadPhase::throwRay(const vxRay &ray) const
 {
@@ -51,6 +173,78 @@ bool vxBroadPhase::throwRay(const vxRay &ray) const
 
 int vxBroadPhase::throwRay(const vxRay &ray, vxCollision &collide) const
 {
+#if	DRAWBBOX
+	if(bb->throwRay(ray, collide))
+	{
+		collide.setColor(m_color);
+		collide.setValid(true);
+
+		return 1;
+	}
+#else
+	if(!m_bb->throwRay(ray, collide))
+	{
+		return 0;
+	}
+	
+	auto&& p = ray.origin();
+	
+	std::vector<vxCollision> cols;
+	auto sp =  collide.position() 
+			+ (collide.normal().inverted() / (scalar)10000.0);
+
+	auto prev = size();
+	bpSearchResult bbxs;
+
+	do
+	{
+		bbxs = getList(ray, sp);
+		
+		if(prev==bbxs.index)
+		{
+			break;
+		}
+		
+		prev = bbxs.index;
+		
+		for(const auto &id: *(bbxs.listRef) )
+		{
+			if(m_triangles[id].throwRay(ray,collide))
+			{
+				if(!ray.direction().follows((collide.position()-ray.origin()).unit()))
+				{
+					cols.push_back(collide);
+				}
+			}
+		}
+	}
+	while(!cols.size());
+
+	if(cols.size())
+	{
+		auto mind  = (cols[0].position()-p).length();
+		collide = cols[0];
+		int i{0};
+		for(auto&& c:cols)
+		{
+			i++;
+			auto ds = (c.position()-p).length();
+			if( ds < mind )
+			{
+				collide = c;
+			}
+		}
+
+		collide.setColor(m_baseColor);
+		collide.setValid(true);
+		collide.setUV(v2(0.5,0.5));
+		return 1;
+	}
+	
+#endif
+	return 0;
+	
+	/*
 //	if(!m_root->throwRay(ray,collide))
 //	{
 //		return 0;
@@ -78,7 +272,7 @@ int vxBroadPhase::throwRay(const vxRay &ray, vxCollision &collide) const
 		}
 	}
 
-	return valid;
+	return valid;*/
 }
 
 bool vxBroadPhase::hasCollision(const vxRay &ray) const
@@ -95,4 +289,3 @@ bool vxBroadPhase::hasCollision(const vxRay &ray) const
 	
 	return false;
 }
-
--- vxBroadPhase.h
+++ vxBroadPhase.h
@@ -8,28 +8,83 @@
 namespace vxCore {
 
 
+/// name to be changed
+struct bpSearchResult
+{
+	unsigned long index;
+	vxGeometryHandle geoRef;
+};
+
 class vxBroadPhase
 {
-	
 	std::vector<vxGeometryHandle> m_geometries;
-	
-	std::shared_ptr<vxBoundingBox> m_root;
-	
-	vxBoundingBoxHandle cameraBB;
+	vxBoundingBoxHandle m_bb;
+	vxBoundingBoxHandle m_cameraBB;
 	
 public:
+
+	///
+	/// \brief vxBroadPhase
+	///Default constructor
 	vxBroadPhase();
 
+	///
+	/// \brief addGeometry
+	/// \param geo
+	///adds a geometry handle to be viewed by broadphase
 	void addGeometry(vxGeometryHandle geo);
-
-
+	
 	vxBoundingBoxHandle closestBox(const v3 &p) const;
+	
+	
+	vxColor m_color{vxColor::white};
 	
 	//renderable interface
 	bool throwRay(const vxRay &ray) const;
 	int throwRay(const vxRay &ray, vxCollision &collide) const;
 	bool hasCollision(const vxRay &ray) const;
+	
+	
+public:
+	///////////////////////////////////////////
+	///// new grid interface.
+	///////////////////////////////////////////
+	
+	static const bpSearchResult invalidResult;
+
+	unsigned int m_rx{1u};
+	unsigned int m_ry{1u};
+	unsigned int m_rz{1u};
+	unsigned int m_c_size{1u};
+	
+	std::vector<scalar> m_xvalues;
+	std::vector<scalar> m_yvalues;
+	std::vector<scalar> m_zvalues;
+
+	std::vector<bpSearchResult> m_members;
+	
+	void updateCache();
+	unsigned long index(unsigned int a,
+						unsigned int b,
+						unsigned int c) const;
+	
+	unsigned int rx() const;
+	void setRx(unsigned int rx);
+
+	unsigned int ry() const;
+	void setRy(unsigned int ry);
+
+	unsigned int rz() const;
+	void setRz(unsigned int rz);
+
+	unsigned long lookupVoxel(const v3 &v, int &a, int &b, int &c) const;
+	void locateAndRegister(vxGeometryHandle geo);
+	const bpSearchResult getList(const vxRay &ray, v3 &sp) const;
+	bool indexIsValid(const long idx) const;
+	bool hasTriangles(const long idx) const;
 };
+
+using vxBroadPhaseHandler = std::shared_ptr<vxBroadPhase>;
 
 }
 #endif // VXBROADPHASE_H
