416fdc90baef86a98c1e3e84d1eb42e489959c25
Mariano Blanc <marianobm@gmail.com>
2016-02-01 20:23:16 +0000

    Fixed issues with the indexes in ranges for broadphase, some were never written.

--- vxBroadPhase.cpp
+++ vxBroadPhase.cpp
@@ -223,22 +223,22 @@ void vxBroadPhase::locateAndRegister(vxGeometryHandle geo)
 		return;
 	}
 	
-	for(auto x=a1;x<a2;x++)
+	for(auto x=a1;x<=a2;x++)
 	{
 		std::cout << "x: " << x << std::endl;
-		for(auto y=b1;y<b2;y++)
+		for(auto y=b1;y<=b2;y++)
 		{
 			std::cout << "\ty: " << y << std::endl;
 			
-			for(auto z=c1;z<c2;z++)
+			for(auto z=c1;z<=c2;z++)
 			{
 				std::cout << "\t\tz: " << z << std::endl;
 
 				auto idx = index(x,y,z);
-				if(idx==1)
+/*				if(idx==1)
 				{
 					std::cerr << "not possible" << std::endl;
-				}
+				}*/
 				
 				if(m_members[idx].geoRefs == nullptr)
 				{
