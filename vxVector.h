01cbbf61c8bbb2412d6c0a403255842249aab30f
Mariano Blanc <marianobm@gmail.com>
2016-02-11 19:11:21 +0000

    Removed int operations with vector overloads.

--- vxVector.h
+++ vxVector.h
@@ -41,16 +41,12 @@ public:
 	vxVector2d unit() const;
 	vxVector2d operator+(const vxVector2d& other) const;
 	vxVector2d operator+(scalar factor) const;
-	vxVector2d operator+(int factor) const;
 	vxVector2d operator-(const vxVector2d& entrada) const;
 	vxVector2d operator-(scalar factor) const;
-	vxVector2d operator-(int factor) const;
 	vxVector2d operator*(const vxVector2d& entrada) const;
 	vxVector2d operator*(scalar factor) const;
-	vxVector2d operator*(int factor) const;
 	vxVector2d operator/(const vxVector2d& entrada) const;
 	vxVector2d operator/(scalar factor) const;
-	vxVector2d operator/(int factor) const;
 	static vxVector2d zero;
 
 	bool operator==(const vxVector2d &other) const;
@@ -81,7 +77,7 @@ protected:
 	scalar m_y {0.0};
 	scalar m_z {0.0};
 
-	//std::valarray<scalar> m_v{0.0,3};
+	scalar m_w {0.0};
 	
 public:
 	
@@ -130,7 +126,6 @@ public:
 	vxVector3d operator+(const vxVector3d &entrada) const;
 	vxVector3d& operator+=(const vxVector3d &entrada);
 	vxVector3d operator+(scalar factor) const;
-	vxVector3d operator+(int factor) const;
 
 	vxVector3d operator-(const vxVector3d &entrada) const;
 	vxVector3d operator-(scalar factor) const;
