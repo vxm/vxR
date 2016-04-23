bd872acb7baf4ce30cd2837dc0a7bb54c2395ec5
Mariano Blanc <marianobm@gmail.com>
2016-04-23 23:40:33 +0100

    Added comments to vxCamera.

--- vxCamera.h
+++ vxCamera.h
@@ -37,38 +37,107 @@ private:
 	std::shared_ptr<const ImageProperties> m_properties;
 
 public:
-	
+	///
+	/// \brief vxCamera
+	/// \param prop
+	///
 	vxCamera(std::shared_ptr<const ImageProperties> prop);
+	///
+	/// \brief vxCamera
+	/// \param position
+	/// \param orientation
+	/// \param focusD
+	/// \param apertureH
+	/// \param apertureV
+	///
 	vxCamera(const v3s &position,
 				const v3s &orientation,
 				scalar focusD,
 				scalar apertureH,
 				scalar apertureV);
-
+	///
+	/// \brief set
+	/// \param position
+	/// \param orientation
+	/// \param focusD
+	/// \param apertureH
+	/// \param apertureV
+	///
 	void set(const v3s &position, 
 			 const v3s &orientation, 
 			 scalar focusD = 1.0, 
 			 scalar apertureH = 0.0, 
 			 scalar apertureV = 0.0);
-	
+	///
+	/// \brief ray
+	/// \param coord
+	/// \param sampler
+	/// \return 
+	///
 	vxRay ray(const v2s &coord, vxSampler &sampler) const;
-	
+	///
+	/// \brief givemeRandRay
+	/// \param coord
+	/// \return 
+	///
 	vxRay givemeRandRay(const v2s &coord);
-
+	///
+	/// \brief next
+	/// \param skip
+	///
 	void next(unsigned int skip = 1u);
-
+	///
+	/// \brief givemeNextRay
+	/// \param imagen
+	/// \param ang
+	/// \return 
+	///
 	vxRay givemeNextRay(const vxContactBuffer &imagen, scalar ang);
-
+	///
+	/// \brief givemeRandomRay
+	/// \param coord
+	/// \return 
+	///
 	vxRay givemeRandomRay(const v2s &coord);
-	
+	///
+	/// \brief prop
+	/// \return 
+	///
 	std::shared_ptr<const ImageProperties> prop() const;
-	
+	///
+	/// \brief setProp
+	/// \param prop
+	///
 	void setProp(std::shared_ptr<const ImageProperties> prop);
+	///
+	/// \brief horizontalAperture
+	/// \return 
+	///
 	scalar horizontalAperture() const;
+	///
+	/// \brief setHorizontalAperture
+	/// \param horizontalAperture
+	///
 	void setHorizontalAperture(scalar horizontalAperture);
+	///
+	/// \brief verticalAperture
+	/// \return 
+	///
 	scalar verticalAperture() const;
+	///
+	/// \brief setVerticalAperture
+	/// \param verticalAperture
+	///
 	void setVerticalAperture(scalar verticalAperture);
+	///
+	/// \brief properties
+	/// \return 
+	///
 	std::shared_ptr<const ImageProperties> properties() const;
+	///
+	/// \brief setProperties
+	/// \param properties
+	///
 	void setProperties(const std::shared_ptr<const ImageProperties> &properties);
 };
 
