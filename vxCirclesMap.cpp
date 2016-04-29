3d8d0e98cf087949019b42100b91cd93138f7572
Mariano Blanc <marianobm@gmail.com>
2016-04-29 20:35:24 +0100

    circles goes back some versions

--- vxCirclesMap.cpp
+++ vxCirclesMap.cpp
@@ -8,19 +8,40 @@ vxCirclesMap::vxCirclesMap()
 
 vxColor vxCirclesMap::compute(const vxCollision &collision) const
 {
-	vxColor outColor(collision.color());
-	
-	auto&& u = collision.u();
-	auto&& v = collision.v();
-	
-	outColor.set(collision.color());
+	vxColor outColor(vxColor::white);
+return outColor;
+	auto dst = collision.uv().distance({.5,.5});
 
+	if(m_circle1)
+	{
+		if(dst>m_radii[0] && dst<m_radii[1])
+		{
+			outColor.set(collision.color());
+		}
+	}
+
+	if(m_circle2)
+	{
+		if(dst>m_radii[1] && dst<m_radii[2])
+		{
+			outColor.set(vxColor::orange);
+		}
+	}
+	
+	if(m_circle3)
+	{
+		if(dst>m_radii[2] && dst<m_radii[3])
+		{
+			outColor.set(collision.color());
+		}
+	}
+	
 	if(m_margin)
 	{
-		if (((u < m_margn || u>(1.-m_margn)) 
-				|| (v<m_margn || v>(1.-m_margn))))
+		if (((collision.u()<m_margn || collision.u()>(1.-m_margn)) 
+				|| (collision.v()<m_margn || collision.v()>(1.-m_margn))))
 		{
-			outColor.set(m_marginColor);
+			outColor.set(vxColor::black);
 		}
 	}
 	
--- vxCirclesMap.h
+++ vxCirclesMap.h
@@ -18,17 +18,17 @@ public:
 public:
 	virtual vxColor compute(const vxCollision &collision) const;
 
-	std::vector<scalar> m_radii = {0.05, 0.2, 0.32, 0.45, 0.5};
-	std::vector<vxColor> m_colors = {vxColor::white, 
+	std::vector<scalar> m_radii = {0.05, 0.09, 0.32, 0.45, 0.5};
+	std::vector<vxColor> m_colors = {vxColor::red, 
 									 vxColor::orange,
-									 vxColor::white,
+									 vxColor::blue,
 									 vxColor::red,
 									 vxColor::black};
 
 	
-	bool m_circle1 {false};
+	bool m_circle1 {true};
 	bool m_circle2 {true};
-	bool m_circle3 {false};
+	bool m_circle3 {true};
 	bool m_margin  {true};
 
 	scalar m_margn = 0.04;
